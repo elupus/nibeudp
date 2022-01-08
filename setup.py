bcda56fe43f6d294c8d552ed207261c7cd3f904f
Joakim Plate <elupus@ecce.se>
2022-01-08 17:46:30 +0100

    Add anyio to dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name="nibeudp", packages=find_packages())
+setup(
+    name="nibeudp",
+    packages=find_packages(),
+    install_requires=[
+        "anyio"
+    ]
+)
